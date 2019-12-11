95f8d99d1f993e73c5c043843cb9fbbffacbb3fb
yasyashu26 <yeshuyeshwanth03@gmail.com>
2019-12-11 12:52:04 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("john cena")
+print("now ! i'll count my chickens")
+print ("hens",25+30/6)
+#print("roosters",100-25*3%4)
+print (3+4-2*8%2)
+#print (5+2<7+6)
+cars=200
+pass_in_car=800
+print("today avaialble cars",cars,"soo much")
+print("passengers are",pass_in_car)
+my_name='john cena'
+print("my name is %s"%my_name)
+
